ac748424554907c585cfad190cf6e18528fa4b08
Artax601 <artaxw1@gmail.com>
2023-12-30 16:18:41 +0100

    Add nomination section as a partial template
    Created a new template partial, nomination-section.php, to display information about the Oscar nomination. This allows easy removal of the section after the event concludes. The partial includes a title and an image related to the Oscar event.

---
+++ app/public/wp-content/themes/foce-child/parts/nomination-section.php
@@ -0,0 +1,8 @@
+<section class="nomination-section">
+    <div class="nomination-content">
+        <h3>Fleurs d’oranger & chats errants est nominé aux Oscars Short Film Animated de 2022 !</h3>
+        <!-- on inclut l'image de l'oscars -->
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/18-courts-metrages-francais-d-animation-eligibles-aux-oscars-2021.png" alt="">
+    </div>
+</section>
+
