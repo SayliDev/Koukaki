5b318e4643cc0b3dc9f4629457a991f7fc9c17cb
Artax601 <artaxw1@gmail.com>
2024-01-09 13:36:34 +0100

    Implemented tablet and desktop responsive design for the entire site

--- app/public/wp-content/themes/foce-child/front-page.php
+++ app/public/wp-content/themes/foce-child/front-page.php
@@ -14,7 +14,7 @@ get_header();
     <section id="story" class="story">
         <?php get_template_part('parts/carousel-characters'); ?>
         <article id="place">
-            <div class="clouds" style="display: none;">
+            <div class="clouds" >
                 <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/big_cloud.png">
                 <img class="little" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/little_cloud.png">
             </div>
